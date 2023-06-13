d8e6e5bf4af0646cdee12e4898a389b132d57cfc
Akshay Sen <akshay.sen.790@gmail.com>
2023-06-13 15:54:53 +0530

    Update code

--- housing/component/data_transformation.py
+++ housing/component/data_transformation.py
@@ -3,7 +3,69 @@ from housing.logger import logging
 from housing.entity.config_entity import DataIngestionConfig, DataTransformationConfig \
     , DataValidationConfig
 from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
-import os, sys
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.pipeline import  Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from housing.util.util import read_yaml_file
+from housing.constant import *
+
+
+class FeatureGenerator(BaseEstimator, TransformerMixin):
+
+    def __init__(self, add_bedrooms_per_room=True,
+                 total_rooms_ix=3,
+                 population_ix=5,
+                 households_ix=6,
+                 total_bedrooms_ix=4, columns=None):
+        '''
+        FeatureGenerator initialization
+        add_bedrooms_per_room=bool,
+        total_rooms_ix=int index number of total rooms columns,
+        population_ix=int index number of total population columns,
+        households_ix=int index number of total households columns,
+        total_bedrooms_ix=int index number of total bedrooms columns
+        '''
+        try:
+            self.columns= columns
+            if self.columns is not None:
+                total_rooms_ix= self.columns.index(COLUMN_TOTAL_ROOMS),
+                population_ix= self.columns.index(COLUMN_POPULATION),
+                households_ix= self.columns.index(COLUMN_HOUSEHOLDS),
+                total_bedrooms_ix = self.columns.index(COLUMN_TOTAL_BEDROOMS)
+
+            self.add_bedrooms_per_room = add_bedrooms_per_room
+            self.total_rooms_ix = total_rooms_ix
+            self.population_ix = population_ix
+            self.households_ix = households_ix
+            self.total_bedrooms_ix = total_bedrooms_ix
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
+        
+    def fit(self, x, y=None):
+        return self
+    
+    def transform(self, x, y =None):
+        try:
+            rooms_per_household = x[:, self.total_rooms_ix] / x[:, self.households_ix]
+            population_per_household = x[:,self.population_ix] / x[:, self.households_ix]
+            
+            if self.add_bedrooms_per_room:
+                bedrooms_per_room = x[:, self.total_bedrooms_ix] / x[:, self.total_rooms_ix]
+                generated_feature = np.c_[ x, rooms_per_household, population_per_household, bedrooms_per_room ]
+            else:
+                generated_feature = np.c_[ x, rooms_per_household, population_per_household ]
+
+            return generated_feature
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
 
 
 class DataTransformation:
@@ -19,3 +81,23 @@ class DataTransformation:
 
         except Exception as e:
             raise HousingException(e, sys) from e
+
+    @staticmethod
+    def load_data(file_path: str, schema_file_path: str)-> pd.DataFrame:
+        try:
+            dataset_schema = read_yaml_file(schema_file_path)
+            schema = dataset_schema[DATASET_SCHEMA_COLUMNS_KEY]
+            dataframe = pd.read_csv(file_path)
+            error_message = ""
+
+            for column in dataframe.columns:
+                if column in list(schema.keys()):
+                    dataframe[column].astype(schema[column])
+                else:
+                    error_message = f"{error_message} \n column: [{column}] is not in the schema."
+
+            if len(error_message)>0:
+                raise Exception(error_message)
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -5,7 +5,7 @@ ROOT_DIR =os.getcwd() # get current working directory
 
 CONFIG_DIR = 'config'
 CONFIG_FILE_NAME ='config.yaml'
-CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_FILE_NAME)
+CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 
 CURRENT_TIME_STAMP = f"{datetime.now().strftime('%y-%m-%d-%H-%M-%S')}"
 
